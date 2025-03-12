afdc9af8729f5f3b0b954c12614579d307056396
Kasper <kasper.allais@gmail.com>
2025-03-12 05:34:48 -0600

    added methods for opening and closing files

--- main.cpp
+++ main.cpp
@@ -3,9 +3,24 @@
 #include <cstring>
 #include <vector>
 #include <cstdlib>
+#include <fstream>
 
 using namespace std;
 
+
+void readTaskList(string fileName) {
+	ofstream file(fileName);
+
+
+}
+
+void writeToTaskList(string fileName) {
+	ofstream file(fileName);
+
+	file << "Hello World";
+
+	file.close();
+}
 
 void addTask(int argc, char* argv[], vector<string>* taskList) {
 	string task;
@@ -21,24 +36,32 @@ void deleteTask(int element, vector<string>* taskList) {
 	taskList->erase(taskList->begin() + element);
 }
 
+void printTaskList(vector<string> taskList) {
+	for (string task : taskList) {
+		cout << task << endl;
+	}
+}
+
 int main(int argc, char* argv[]) {
+
+	writeToTaskList("test.txt");
 
 	vector<string> taskList;
 
-	// Add Task
+	// Add a Task
 	if (argc > 1 && strcmp(argv[1], "add") == 0) {
 		addTask(argc, argv, &taskList);
 	}
 
+	// Delete a Task
 	if (argc > 1 && strcmp(argv[1], "delete") == 0) {
 		deleteTask(atoi(argv[2]), &taskList);
 	}
 
-	for (string task: taskList) {
-		cout << task;
-	}
-
-	deleteTask(0, &taskList);
+	// Print Task List
+	if (argc > 1 && strcmp(argv[1], "list") == 0) {
+		printTaskList(taskList);
+	} 
 
 	return 0;
 }
